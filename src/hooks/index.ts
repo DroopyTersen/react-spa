23949adc3704677c5f9305dd67c27336c27f6a39
Andrew Petersen <andrew.petersen@corebts.com>
2021-04-29 15:06:11 -0600

    removed the missing useQueryString reference

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
 export * from "./useDebounce";
 export { default as useAsyncData } from "./useAsyncData";
 export { default as useInterval } from "./useInterval";
-export { default as useQueryString } from "./useQueryString";
 export { default as usePersistedState } from "./usePersistedState";
 export { default as useAutoPaging } from "./useAutoPaging";
 export { default as useComponentSize } from "./useComponentSize";
